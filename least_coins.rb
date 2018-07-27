f7fe620bbf5ac7a2b0fd5b666da9d48623e1fe47
bchamarthi <brchamarthi29@gmail.com>
2018-07-27 12:42:56 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -11,8 +11,27 @@ items["quarter"] = 25
 items["dime"] = 10
 items["nickel"] = 5 
 items["penny"] = 1 
-
-  return {:quarter => , :dime => , :nickel => , :penny => } 
+quarters = 0 
+dimes = 0 
+nickels = 0 
+pennies = 0 
+until cents < 25
+quarters += 1
+cents = cents - 25
+end
+until cents < 10
+dimes += 1 
+cents = cents - 10 
+end
+until cents < 5 
+nickels += 1 
+cents = cents - 5 
+end 
+until cents < 1 
+pennies += 1 
+cents = cents - 1
+end
+  return {:quarter => quarters, :dime => dimes, :nickel => nickels, :penny => pennies} 
   
 end
 
