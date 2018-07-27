6fb00852ed0c67c589624c4c3cec5f57cd87559e
bchamarthi <brchamarthi29@gmail.com>
2018-07-27 12:37:03 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,13 +1,7 @@
 #write out your code here
 puts "How many cents do you have?"
 cents = gets.chomp.to_i 
-  
-  quarter = 25
-  dime = 10 
-  nickel = 5
-  penny = 1
-  
-  
+
 
 def least_coins(cents)
 
@@ -17,10 +11,7 @@ items["quarter"] = 25
 items["dime"] = 10
 items["nickel"] = 5 
 items["penny"] = 1 
-until total == cents
 
-
-end
   return {:quarter => , :dime => , :nickel => , :penny => } 
   
 end
